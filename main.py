a7eb3b62fad8afe691e5fdbc03184386f47f6f08
Sami Laine <sami.v.laine@gmail.com>
2018-12-29 11:37:57 +0100

    Reorder main.py

--- main.py
+++ main.py
@@ -11,16 +11,9 @@ if cfg.VERSION != CONFIG_VERSION:
 
 initial_extensions = ['commandListener']
 
-bot = commands.Bot(command_prefix=(cfg.COMMAND_CHAR))
+bot = commands.Bot(command_prefix=cfg.COMMAND_CHAR)
 
 bot.remove_command("help")
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        # try:
-            bot.load_extension(extension)
-        # except Exception as e:
-        #     print(f'failed to load extension {extension}')
 
 @bot.event
 async def on_ready():
@@ -30,4 +23,12 @@ async def on_ready():
     print(bot.user.id)
     await bot.change_presence(activity=discord.Game(name=cfg.GAME, type=2))
 
-bot.run(TOKEN)
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        # try:
+            bot.load_extension(extension)
+        # except Exception as e:
+        #     print(f'failed to load extension {extension}')
+
+
+    bot.run(TOKEN)
