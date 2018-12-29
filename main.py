0fce98d483a7a32183c2b15fdfc3626766c4bf37
Sami Laine <sami.v.laine@gmail.com>
2018-12-29 12:49:29 +0200

    Satisfy flake8

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3.7
 import discord
-import asyncio
 from discord.ext import commands
 import config as cfg
 from secret import TOKEN
 
 CONFIG_VERSION = 3
 if cfg.VERSION != CONFIG_VERSION:
-    raise Exception("Outdated config file, expecting version {}, found version {}".format(CONFIG_VERSION, cfg.VERSION))
+    raise Exception(
+        "Outdated config file, expecting version {}, found version {}"
+        .format(CONFIG_VERSION, cfg.VERSION))
 
 initial_extensions = ['commandListener']
 
 bot = commands.Bot(command_prefix=cfg.COMMAND_CHAR)
-
 bot.remove_command("help")
+
 
 @bot.event
 async def on_ready():
@@ -29,6 +30,5 @@ if __name__ == '__main__':
             bot.load_extension(extension)
         # except Exception as e:
         #     print(f'failed to load extension {extension}')
-
 
     bot.run(TOKEN)
--- role.py
+++ role.py
@@ -1,8 +1,3 @@
-import discord
-import sqlite3
-
-from discord.ext import commands
-
 class Role:
 
     def __init__(self, name, emote):
