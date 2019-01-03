e35d586d5d3f4811a3d3acf6e317e92cc5a14462
Sami Laine <sami.v.laine@gmail.com>
2019-01-03 21:59:12 +0200

    Fix the reload command
    The reload extension must not be unloaded during a reload. Otherwise, a
    syntax error during a reload would leave the reload command undefined,
    which would prevent future reloads.

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ if cfg.VERSION != CONFIG_VERSION:
         "Outdated config file, expecting version {}, found version {}"
         .format(CONFIG_VERSION, cfg.VERSION))
 
-initial_extensions = ['commandListener', 'eventListener', 'reload']
+initial_extensions = ['commandListener', 'eventListener']
 bot = commands.Bot(command_prefix=COMMAND_CHAR)
 bot.remove_command("help")
 eventDatabase_ = eventDatabase.EventDatabase()
 
 if __name__ == '__main__':
+    bot.load_extension('reload')
     for extension in initial_extensions:
         try:
             bot.load_extension(extension)
