26764d6d654731275ba7065263487a1dc2e220db
MaxKosi <m_kosi@yahoo.com>
2021-03-28 15:13:45 +0200

    improved show command

--- commandListener.py
+++ commandListener.py
@@ -207,6 +207,8 @@ class CommandListener(Cog):
 
     @command(aliases=["cat"])
     async def show(self, ctx: Context, event: EventEvent):
+        message = await self.bot.eventchannel.fetch_message(event.messageID)
+        await ctx.send(message.jump_url)
         await msgFnc.createEventMessage(event, ctx.channel, update_id=False)
 
     # Create event command
