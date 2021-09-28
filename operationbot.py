046bc142c5b75856c7364f5fe4d5bb9b40e747f9
Sami Laine <sami.v.laine@gmail.com>
2021-09-28 19:43:56 +0200

    Add bot-wide error handler

--- operationbot.py
+++ operationbot.py
@@ -2,6 +2,8 @@
 
 import logging
 import sys
+import traceback
+
 import discord
 from discord import TextChannel, User
 from discord.ext.commands import Bot, DefaultHelpCommand
@@ -115,3 +117,19 @@ class OperationBot(Bot):
             logging.exception("Error loading database")
             await self.close()
             sys.exit()
+
+    async def on_error(self, event_method, *args, **kwargs):
+        print(f"Error in {event_method=}")
+        traceback.print_exc()
+        trace = traceback.format_exc(2)
+        _, error, _ = sys.exc_info()
+
+        ctx = self.commandchannel
+        msg = (f"Unexpected error occured in {event_method}: ```{error}```\n"
+               f"```py\n{trace}```")
+        if len(msg) >= 2000:
+            await ctx.send("Received error message that's over 2000 "
+                           "characters, check log.")
+            print(msg)
+        else:
+            await ctx.send(msg)
