1c32a17a03267685f04aeeb1e69c9009f07ff441
Sami Laine <sami.v.laine@gmail.com>
2022-09-06 22:53:06 +0200

    Enable message content intent

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ initial_extensions = ['commandListener', 'eventListener', 'cogs.repl']
 
 intents = discord.Intents.default()
 intents.members = True  # pylint: disable=assigning-non-slot
+intents.messages = True  # pylint: disable=assigning-non-slot
 bot = OperationBot(command_prefix=COMMAND_CHAR, intents=intents)
 # bot.remove_command("help")
 
