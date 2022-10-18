4d80eb07dfde43592b5a2ef835317a60c722bbab
Sami Laine <sami.v.laine@gmail.com>
2022-10-18 19:52:45 +0000

    scripts/backup: Remove duplicate code, lint

--- scripts/backup-events.sh
+++ scripts/backup-events.sh
@@ -5,60 +5,46 @@ set -euo pipefail
 BOT_LOCATION=$HOME/operationbot
 BACKUP_LOCATION=$BOT_LOCATION/backup
 
-location=$BACKUP_LOCATION/events
-current_file=$BOT_LOCATION/database/events.json
+compare() {
+  name="$1"
 
-mkdir -p $location
+  echo "Checking $name"
 
-backup_date=$(date +\%F-T\%H-\%M-\%S)
-filename=events.json-$backup_date.bak
-new_file=$location/$filename
+  location="$BACKUP_LOCATION/$name"
+  current_file="$BOT_LOCATION/database/$name.json"
 
-previous_file=$(ls $location/events.json* -1 | tail -n 1)
+  mkdir -p "$location"
 
-current_sum=$(md5sum $current_file | cut -d' ' -f 1)
-previous_sum=$(gunzip -c $previous_file | md5sum - | cut -d' ' -f 1)
+  backup_date=$(date +%F-T%H-%M-%S)
+  filename="$name.json-$backup_date.bak"
+  new_file=$location/$filename
 
-echo -n "Current: "
-echo $current_sum
-echo -n "Previous: "
-echo $previous_sum
+  if [ ! -e "$current_file" ]; then
+    >&2 echo "no current file, nothing to do"
+    return
+  fi
 
-if [ $current_sum != $previous_sum ]; then
-  echo "No match"
-  cp $current_file $new_file
-  gzip $new_file
-else
-  echo "Match"
-fi
+  previous_file="$(find "$location" -name "$name.json*" | sort | tail -n 1)"
+  if [ -z "$previous_file" ]; then
+    echo "no previous file"
+    no_previous=yes
+  else
+    current_sum=$(md5sum "$current_file" | cut -d' ' -f 1)
+    previous_sum=$(gunzip -c "$previous_file" | md5sum - | cut -d' ' -f 1)
 
+    echo "Current: $current_sum"
+    echo "Previous: $previous_sum"
+  fi
 
-location=$BACKUP_LOCATION/archive
-current_file=$BOT_LOCATION/database/archive.json
-
-mkdir -p $location
-
-backup_date=$(date +\%F-T\%H-\%M-\%S)
-filename=archive.json-$backup_date.bak
-new_file=$location/$filename
-
-previous_file=$(ls $location/archive.json* -1 | tail -n 1)
-
-current_sum=$(md5sum $current_file | cut -d' ' -f 1)
-previous_sum=$(gunzip -c $previous_file | md5sum - | cut -d' ' -f 1)
-
-echo -n "Current: "
-echo $current_sum
-echo -n "Previous: "
-echo $previous_sum
-
-if [ $current_sum != $previous_sum ]; then
-  echo "No match"
-  cp $current_file $new_file
-  gzip $new_file
-else
-  echo "Match"
-fi
-#cp ~/operationbot/eventDatabase.json ~/operationbot/backup/eventDatabase.json-$(date +\%F-T\%H-\%M-\%S).bak
+  if [ "${no_previous:-no}" = "yes" ] || [ "$current_sum" != "$previous_sum" ]; then
+    echo "No match"
+    cp "$current_file" "$new_file"
+    gzip "$new_file"
+  else
+    echo "Match"
+  fi
+}
 
 
+compare events
+compare archive
